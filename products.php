4e2c287bc798ef91bf7a46048681c735ae18c4c1
saimah14 <saimah122267@gmail.com>
2026-09-21 06:08:50 -0700

    Create products.php

---
+++ products.php
@@ -0,0 +1,30 @@
+<?php
+
+$products = [
+    [
+        "id" => "P001",
+        "nama" => "Indomie",
+        "kategori" => "Makanan",
+        "harga" => 3500,
+        "stok" => 10,
+        "deskripsi" => "Mi instan"
+    ],
+    [
+        "id" => "P002",
+        "nama" => "Aqua",
+        "kategori" => "Minuman",
+        "harga" => 4000,
+        "stok" => 2,
+        "deskripsi" => "Air mineral"
+    ],
+    [
+        "id" => "P003",
+        "nama" => "Roti",
+        "kategori" => "Makanan",
+        "harga" => 5000,
+        "stok" => 8,
+        "deskripsi" => "Roti tawar"
+    ]
+];
+
+?>
